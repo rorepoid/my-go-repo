82d586973ddd2594a3486364970ef662db5580f4
Roberto <rorellanaa@ucvvirtual.edu.pe>
2020-06-17 23:58:55 -0500

    feat: use variables

--- hello.go
+++ hello.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+var (
+	name string = "Roberto"
+	location string
+	age int
+)
+
+
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("My name is: " + name + "\n")
 }
